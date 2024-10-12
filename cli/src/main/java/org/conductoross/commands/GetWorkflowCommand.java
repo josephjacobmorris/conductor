4c30a827e58c9cd0699d75a53e82818cf1152718
Joseph Jacob <josephjacob1998@gmail.com>
2024-10-12 12:19:54 +0530

    Conductor CLI: add workflow get-workflow-execution part 1

---
+++ cli/src/main/java/org/conductoross/commands/GetWorkflowCommand.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2024 Conductor Authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.conductoross.commands;
+
+import org.springframework.shell.command.annotation.Command;
+
+@Command(command = "workflow")
+public class GetWorkflowCommand {
+    @Command(command = "get-workflow-execution")
+    public String getWorkflowExecution() {
+        // TODO add call to conductor and then format
+        return "workflowId";
+    }
+}
