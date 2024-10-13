fc5bab039ea06828b1e9b84b83fbbe952e39fe07
Joseph Jacob <josephjacob1998@gmail.com>
2024-10-13 10:17:51 +0530

    Conductor CLI: add conductor client exception handling part 2

--- cli/src/main/java/org/conductoross/cli/utils/Formatter.java
+++ cli/src/main/java/org/conductoross/cli/utils/Formatter.java
@@ -1,12 +1,22 @@
+/*
+ * Copyright 2024 Conductor Authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.conductoross.cli.utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
-
 public class Formatter {
-    public static String printDefault(Object object) {
+    public static String printAsYaml(Object object) {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode rootNode = mapper.convertValue(object, JsonNode.class);
         StringBuilder sb = new StringBuilder();
@@ -16,16 +26,19 @@ public class Formatter {
 
     private static void printJsonNode(JsonNode node, String indent, StringBuilder sb) {
         if (node.isObject()) {
-            node.fieldNames().forEachRemaining(fieldName -> {
-                System.out.print(indent + fieldName + ": ");
-                printJsonNode(node.get(fieldName), indent + "  ", sb);
-            });
+            node.fieldNames()
+                    .forEachRemaining(
+                            fieldName -> {
+                                sb.append(indent + fieldName + ": ");
+                                printJsonNode(node.get(fieldName), indent + "  ", sb);
+                            });
         } else if (node.isArray()) {
+
             for (JsonNode element : node) {
                 printJsonNode(element, indent, sb);
             }
         } else {
-            System.out.println(node.asText());
+            sb.append(node.asText() + System.lineSeparator());
         }
     }
 }
